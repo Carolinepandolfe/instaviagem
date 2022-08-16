6fd05f1fcfc372ffce5423ca68123e70e9f0a829
carolpandolfe <carolinepandolfe@yahoo.com.br>
2022-08-15 21:42:33 -0300

    first commit

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,11 @@
+import 'styled-components'
+
+declare module 'styled-components' {
+	export interface DefaultTheme {
+		base: {
+			blue: string
+			black: string
+			white: string
+		}
+	}
+}
---
+++ src/styles/theme/default.ts
@@ -0,0 +1,9 @@
+import { DefaultTheme } from 'styled-components'
+
+export const defaultTheme: DefaultTheme = {
+	base: {
+		blue: '#3fd0d4',
+		black: '#212121',
+		white: '#ffff',
+	},
+}
