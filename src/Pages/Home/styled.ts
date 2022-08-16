c903f0d445e800d94a5b62d9ad7971fb6f1c0c62
carolpandolfe <carolinepandolfe@yahoo.com.br>
2022-08-16 20:56:59 -0300

    Input search at home

---
+++ src/Pages/Home/styled.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+	display: grid;
+	flex-direction: column;
+	padding: 20px 14px 20px;
+	max-width: 1200px;
+	margin: 0 auto;
+`
