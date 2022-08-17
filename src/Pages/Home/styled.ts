66722e299f1b17bd16dc7c30673d61ae12cf277b
carolpandolfe <carolinepandolfe@yahoo.com.br>
2022-08-17 08:51:40 -0300

    Create card with information

--- src/Pages/Home/styled.ts
+++ src/Pages/Home/styled.ts
@@ -3,7 +3,9 @@ import styled from 'styled-components'
 export const Container = styled.div`
 	display: grid;
 	flex-direction: column;
-	padding: 20px 14px 20px;
-	max-width: 1200px;
+	gap: 30px;
+	justify-content: center;
 	margin: 0 auto;
+	max-width: 1200px;
+	padding: 20px 14px 20px;
 `
